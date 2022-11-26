29c87227dda06d9ef956d1f7ea160fea70c3c037
Marco Grassi <marco.gra@gmail.com>
2022-11-26 12:29:59 +0800

    fix the description string property of flash.net.FileFilter which should follow the format such as Images (*.gif, *.jpg, *.png)

--- core/src/avm2/globals/flash/net.rs
+++ core/src/avm2/globals/flash/net.rs
@@ -1,8 +1,8 @@
 //! `flash.net` namespace
 
-pub mod object_encoding;
-pub mod sharedobject;
-pub mod url_loader;
 pub mod file_filter;
 pub mod file_reference;
 pub mod file_reference_list;
+pub mod object_encoding;
+pub mod sharedobject;
+pub mod url_loader;
